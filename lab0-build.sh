cec3a3fb2f8b366764a156556606cfe44e0d90a4
powcoder代写 <powcoder@163.com>
2022-07-02 13:02:05 +0800

    程序代写 加微信 powcoder

---
+++ lab0-build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd ~/lab0/build
+cmake ..
+make
+
+cd ~/lab0/test
+clang -emit-llvm -S -fno-discard-value-names -c simple0.c
+
+cd ~
+opt -load lab0/build/PrereqPass.so -Prereqs -disable-output lab0/test/simple0.ll
+
+cd ~/lab0
+
+exit 
